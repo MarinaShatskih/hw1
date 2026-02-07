4be22861bd05fd97b262b82b92111dedbd4b7f3d
Marina <butkevich.marina7896@gmail.com>
2026-02-07 15:58:42 +0700

    test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,26 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-class CashbackHackServiceTest {
+
+
+public class CashbackHackServiceTest {
+
     @Test
-    public void shouldReturnCorrectRemaining() {
+    public void testRemain_WhenAmountLessThanBoundary() {
         CashbackHackService service = new CashbackHackService();
-
-        int amount = 900;
-        int actual = service.remain(amount);
+        int actual = service.remain(900);
         int expected = 100;
-
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void testRemain_WhenAmountExactlyBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 1000;
+        Assert.assertEquals(actual, expected);
+    }
+
 }
